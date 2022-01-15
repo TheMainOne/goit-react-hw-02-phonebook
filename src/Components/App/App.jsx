168099f91b8228ffadfb1b3a6891550f521d84a1
Maksym Lvov <leva880@ukr.net>
2022-01-15 16:02:53 +0300

    did task 3

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,40 +1,63 @@
 import React, { Component } from "react";
 import { nanoid } from "nanoid";
+import Contacts from "../Contacts/Contacts";
 
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ],
-    filter: '',
-    name: '',
-    number: ''
+    filter: "",
+    name: "",
+    number: "",
   };
 
-  handleSubmit = (event) => {
+  onHandleSubmit = (event) => {
     const form = event.target;
     const contactName = event.target[0].value;
-    const contactPhone = event.target[1].value
+    const contactPhone = event.target[1].value;
     event.preventDefault();
 
-    this.setState(prevState => {
-      console.log(prevState);
-      return { contacts: [...prevState.contacts, { name: contactName, number: contactPhone }] }
-    }); 
-    
-    console.log(this);
+    this.setState((prevState) => {
+      return {
+        contacts: [
+          ...prevState.contacts,
+          { id: nanoid(), name: contactName, number: contactPhone },
+        ],
+      };
+    });
+
     form.reset();
   };
 
+  onSearchInput = (event) => {
+    const inputValue = event.target.value;
+
+    this.setState({ filter: inputValue });
+  };
+
+  getFilteredContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(
+      (contact) =>
+        contact.name.toLowerCase().includes(normalizedFilter) ||
+        contact.number.includes(normalizedFilter)
+    );
+  };
+
   render() {
+    const filteredContacts = this.getFilteredContacts();
+
     return (
       <>
         <h1>Phonebook</h1>
         <div className="phonebook">
-          <form onSubmit={this.handleSubmit}>
+          <form onSubmit={this.onHandleSubmit}>
             <label>
               write your name here
               <input
@@ -59,13 +82,13 @@ class App extends Component {
           </form>
         </div>
         <div className="contacts">
-          <ul>
-            {this.state.contacts
-              ? this.state.contacts.map(({name, number}) => (
-                <li key={nanoid()}>{name}: {number} </li>
-                ))
-              : false}
-          </ul>
+          <input
+            type="text"
+            value={this.state.filter}
+            onChange={this.onSearchInput}
+          />
+
+          <Contacts contacts={this.state.contacts} filteredContacts={filteredContacts}></Contacts>
         </div>
       </>
     );
---
+++ src/Components/Contacts/Contacts.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Contacts({ contacts, filteredContacts }) {
+  return (
+    <div>
+      {contacts.length === 0 ? (
+        <p>No contacts added yet</p>
+      ) : (
+        <ul>
+          {filteredContacts.length === 0 ? (
+            <p>Contact not found</p>
+          ) : (
+            filteredContacts.map(contact => (
+              <li key={contact.id}>
+                <p>
+                  {contact.name}: {contact.number}
+                </p>
+              </li>
+            ))
+          )}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default Contacts;
